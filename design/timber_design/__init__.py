4f4661287cf0f27d953fa1df90e3d1739adff707
Michael Park <minpar@student.ethz.ch>
2026-03-29 11:57:00 +0200

    Reciprocal_Frame_Clusters

---
+++ design/timber_design/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Reciprocal Frame from COMPAS Mesh Dual
+
+A COMPAS-based solver for generating reciprocal frame structures from triangulated meshes.
+"""
+
+
+from .core_graph import ReciprocalFrame
+
+__all__ = ["ReciprocalFrame"]
