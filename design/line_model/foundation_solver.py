f64dda1abe9601e11df9bda3a4fd515b5051c62d
jerrywang <jerry84562890727@gmail.com>
2026-05-07 14:19:40 +0200

    assign 'cut_plane' to support node attributes

--- design/line_model/foundation_solver.py
+++ design/line_model/foundation_solver.py
@@ -239,9 +239,12 @@ def trim_anchored_beams_and_find_cut_planes(graph, min_radius=0.60, min_height=0
         cro_secs[sup_node] = crosecs
         steps[sup_node] = step
 
+        # 3. Assign cut planes to nodes' attrubutes in graph
         sup_pt = graph.node_point(sup_node)
         cut_pln = Plane(sup_pt + Vector(0, 0, step), Vector(0, 0, 1))
         cuts[sup_node] = cut_pln
+        
+        graph.node_attribute(sup_node, "cut_plane", cut_pln)
         
     return {
         "cross_sections": cro_secs,
