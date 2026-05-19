fc0c690c5a02cd7694929a135318a49339c1e7f4
ananya <kango@arch.ethz.ch>
2026-05-19 10:31:24 +0200

    merge

--- fabrication/robot/timber/timber_planner.py
+++ fabrication/robot/timber/timber_planner.py
@@ -218,14 +218,14 @@ class TimberProcessPlanner(BaseRobotPlanner):
     def global_constraints(self):
         constraints = []
         # constraints.append(JointConstraint('robot12_joint_1', math.radians(90), math.radians(85), math.radians(90), 1.0))
-        # constraints.append(JointConstraint('robot12_joint_2', math.radians(10), math.radians(90), math.radians(90), 1.0))
-        # constraints.append(JointConstraint('robot12_joint_3', math.radians(55), math.radians(10), math.radians(210), 1.0))
+        constraints.append(JointConstraint('robot12_joint_2', math.radians(-55), math.radians(10), math.radians(10), 10.0))
+        constraints.append(JointConstraint('robot12_joint_3', math.radians(55), math.radians(10), math.radians(50), 1.0))
         # constraints.append(JointConstraint('robot12_joint_4', math.radians(180), math.radians(270), math.radians(270), 0.5))
         # constraints.append(JointConstraint('robot12_joint_5', math.radians(90), math.radians(25), math.radians(180), 0.5))
         # constraints.append(JointConstraint('robot12_joint_6', 0.0, math.radians(360), math.radians(360), 0.5))
         # constraints.append(JointConstraint('bridge1_joint_EA_X', 13, 2, 4, 1.0))
         constraints.append(JointConstraint('robot12_joint_EA_Y', -7, 4.5, 5, 1.0))
-        constraints.append(JointConstraint('robot12_joint_EA_Z', -3, 2, 1, 0.7))
+        constraints.append(JointConstraint('robot12_joint_EA_Z', -2.5, 2, 2, 0.7))
         # BV = BoundingVolume.from_mesh(Mesh.from_stl(compas.get("C:\\Users\\paulj\\github\\fall_demo_2025\\data\\models\\bounding_volume.stl")))
         # constraints.append(PositionConstraint('robot12_link_6', BV, 1.0))
         return constraints
