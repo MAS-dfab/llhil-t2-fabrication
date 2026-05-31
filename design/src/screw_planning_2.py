81c297c38e65d1e898bca6d327246b1c2a79b7e2
J3rryWan9 <jerry84562890727@gmail.com>
2026-06-01 01:32:23 +0200

    WIP screw planning

--- design/src/screw_planning_2.py
+++ design/src/screw_planning_2.py
@@ -45,20 +45,49 @@ class ScrewSolver:
     def get_specification(self, joint):
         return self._spec_cache[joint.entry_type]
     
-    def determine_pair_count(self, joint, orientation):
-        pts_entry, _ = joint.find_screw_boundaries(orientation=orientation, data_type="points")
-        entry_type = joint.entry_type
-
-        if entry_type == "aligned":
-            dist = pts_entry[0].distance_to_point(pts_entry[3])  # width of the entry face
-        elif entry_type == "crossed":
-            dist = pts_entry[0].distance_to_point(pts_entry[1])  # length of the entry face
-
+    def calculate_screw_capacity(self, joint, orientation):
+        """Calculate the maximum amount of screws in width and length direction."""
         spec = self.get_specification(joint)
-        table = sorted(spec.spec_table["min_widths"].items(), key=lambda x: x[0], reverse=True)
-        return next((pair_count for pair_count, min_width in table if dist >= min_width), 0)
+
+        entry_corners, _ = joint.find_screw_boundaries(orientation=orientation, data_type="points")
+        len_l = entry_corners[0].distance_to_point(entry_corners[1])
         
-    def populate_aligned_entry_points(self, joint, orientation="bisector", nested=False):
+        if joint.entry_type == "aligned":
+            max_w_num = 2  # In our project it's always two screws (row) along width (25, 50, 25)
+
+            dire = joint.calculate_screw_direction(orientation=orientation)
+            main_dire = -joint.point_centerline_towards_joint(joint.main_beam)
+            angle = angle_vectors(dire, main_dire)
+            l_spacing = spec.a1 / math.sin(angle)
+            max_l_num = int(len_l // l_spacing)
+
+        elif joint.entry_type == "crossed":
+            max_w_num = 2  # Left and right each along width
+            table = sorted(spec.spec_table["min_widths"].items(), key=lambda x: x[0], reverse=True)
+            max_l_num = next((pair_count for pair_count, min_width in table if len_l >= min_width), 0)
+
+        else:
+            raise ValueError("Unknown entry type.")
+        return max_w_num, max_l_num
+    
+    def populate_entry_points(self, joint, amount, orientation):
+        """Populate entry points by a given amount of screws."""
+        w_num, max_l_num = self.calculate_screw_capacity(joint, orientation)
+        if amount > w_num * max_l_num:
+            raise ValueError(f"Too many screws requested. Max capacity is {w_num * max_l_num}.")
+        
+        l_num = amount // w_num + (1 if amount % w_num > 0 else 0)
+
+        entry_corners, _ = joint.find_screw_boundaries(orientation=orientation, data_type="points")
+        vec_w = (entry_corners[3] - entry_corners[0]).unitized()
+        vec_l = (entry_corners[1] - entry_corners[0]).unitized()
+
+        if joint.entry_type == "aligned":
+            spec = self._spec_cache["aligned"]
+            w_steps = (spec.a2_cg, spec.a2, spec.a2_cg)
+        pass  # TODO: implement the a1_cg (65mm) check with "aligned" entry type, 60/60 mm penetration for both entry types.
+
+    def populate_aligned_entry_points(self, joint, orientation="bisector"):
         """
         Populate screw points on the entry face.
 
@@ -68,12 +97,11 @@ class ScrewSolver:
             The joint to populate screws on.
         orientation : str
             "perp_main", "perp_riser", "perp_cross", "along_cross", and "bisector".
-        nested : bool
-            If True, create a nested grid with the width list in the inner loop.
         
         Returns
         -------
-        list of list of Point if nested, otherwise a flat list of Point.
+        list of list of Point
+            Inner list is along the width direction.
         """
         if joint.entry_type != "aligned":
             raise ValueError("Wrong entry type. Expected 'aligned'.")
@@ -81,17 +109,12 @@ class ScrewSolver:
 
         pts_entry, _ = joint.find_screw_boundaries(orientation=orientation, data_type="points")
 
-        w_num = self.determine_pair_count(joint, orientation) + 1
-        if w_num != 3:
+        w_num, l_num = self.calculate_screw_capacity(joint, orientation)
+        if w_num != 2:
             raise NotImplementedError("Only support 2 pairs for now.")
         w_steps = (spec.a2_cg, spec.a2, spec.a2_cg)
 
-        dire = joint.calculate_screw_direction(orientation=orientation)
-        main_dire = -joint.point_centerline_towards_joint(joint.main_beam)
-        angle = angle_vectors(dire, main_dire)
-        l_spacing = spec.a1 / math.sin(angle)
         l = pts_entry[0].distance_to_point(pts_entry[1])
-        l_num = int(l // l_spacing)
         l_step = l / l_num
 
         vec_w = (pts_entry[3] - pts_entry[0]).unitized()
@@ -101,18 +124,58 @@ class ScrewSolver:
         start = pts_entry[0] + (vec_w * w_steps[0]) + (vec_l * l_step)
 
         # 3. Create point grid
-        point_grid = [[None for _ in range(w_num - 1)] for _ in range(l_num - 1)]
+        point_grid = [[None for _ in range(w_num)] for _ in range(l_num - 1)]
         for i in range(l_num - 1):
             curr_w = 0.0
-            for j in range(w_num - 1):
+            for j in range(w_num):
                 point_grid[i][j] = start + (vec_w * curr_w) + (vec_l * i * l_step)
                 curr_w += w_steps[j + 1]
-
-        if not nested:
-            point_grid = [pt for row in point_grid for pt in row]
         return point_grid
     
-    def populate_aligned_screw_lines(self, joint, orientation="bisector", nested=False, restrict=True):
+    def populate_crossed_entry_points(self, joint, orientation="along_cross"):
+        """
+        Populate screw points on the entry face from the height sides.
+
+        Parameters
+        ----------
+        joint : JointWrapper
+            The joint to populate screws on.
+        orientation : str
+            "perp_main", "perp_riser", "perp_cross", "along_cross", and "bisector".
+
+        Returns
+        -------
+        list of list of Point
+            Inner list is along the width direction.
+        """
+        if joint.entry_type != "crossed":
+            raise ValueError("Wrong entry type. Expected 'crossed'.")
+        spec = self._spec_cache["crossed"]
+
+        # Find offset entry points
+        corners = joint.get_interface_boundary(data_type="points")
+        offset_dire = joint.calculate_screw_direction(orientation=orientation)
+        amp = spec.side_offset / math.sin(math.radians(joint.acute_angle))
+        offset_vec = offset_dire * amp
+
+        pts_entry = [p + offset_vec for p in corners]
+
+        _, l_num = self.calculate_screw_capacity(joint, orientation)
+        if l_num > 3:
+            raise NotImplementedError("Only support up to 3 pairs for now.")
+        
+        l = pts_entry[0].distance_to_point(pts_entry[1])
+        a2_cg = (l - spec.a2_red - spec.a1 * (l_num - 1)) / 2  # screw center to edge
+
+        vec_l = (pts_entry[1] - pts_entry[0]).unitized()
+        point_grid = []
+        for i in range(l_num):
+            pt_left = pts_entry[0] + (vec_l * a2_cg) + (vec_l * spec.a1) * i
+            pt_right = pts_entry[3] + (vec_l * a2_cg) + (vec_l * spec.a1) * i + (vec_l * spec.a2_red)
+            point_grid.append([pt_left, pt_right])
+        return point_grid
+
+    def populate_aligned_screw_lines(self, joint, point_grid, orientation="bisector", restrict=True):
         """
         Populate screw lines from the entry face to the exit face.
 
@@ -120,23 +183,21 @@ class ScrewSolver:
         ----------
         joint : JointWrapper
             The joint to populate screws on.
+        point_grid : list of list of Point
+            The entry points to create screw lines.
         orientation : str
             "perp_main", "perp_riser", "perp_cross", "bisector" and "along_cross".
-        nested : bool
-            If True, create a nested grid with the width list in the inner loop.
         restrict : bool
             If True, restrict the screw length to the predefined lengths.
         
         Returns
         -------
-        list of list of Lines if nested, otherwise a flat list of Lines.
+        list of list of Lines
+            Inner list is along the width direction.
         """
         if joint.entry_type != "aligned":
             raise ValueError("Wrong entry type. Expected 'aligned'.")
         spec = self._spec_cache["aligned"]
-
-        # 1. Get the entry point grid
-        entry_grid = self.populate_aligned_entry_points(joint, orientation=orientation, nested=True)
 
         # 2. Create exit frame and screw direction
         _, pts_exit = joint.find_screw_boundaries(orientation=orientation, data_type="points")
@@ -145,8 +206,8 @@ class ScrewSolver:
 
         # 3. get the length of each screw line by projecting the point to the exit face
         screw_lengths = sorted(spec.SCREW_LENGTHS, reverse=True)
-        line_list = []
-        for row in entry_grid:
+        line_grid = []
+        for row in point_grid:
             line_row = []
 
             if restrict:
@@ -166,60 +227,10 @@ class ScrewSolver:
                 for pt_entry in row:
                     pt_exit = project_point_to_frame_along(pt_entry, dire, exit_frame)
                     line_row.append(Line(pt_entry, pt_exit))
-            line_list.append(line_row)
+            line_grid.append(line_row)
+        return line_grid
 
-        if not nested:
-            line_list = [line for row in line_list for line in row]
-        return line_list
-
-    def populate_crossed_entry_points(self, joint, orientation="along_cross", nested=False):
-        """
-        Populate screw points on the entry face from the height sides.
-
-        Parameters
-        ----------
-        joint : JointWrapper
-            The joint to populate screws on.
-        orientation : str
-            "perp_main", "perp_riser", "perp_cross", "along_cross", and "bisector".
-        nested : bool
-            If True, create a nested grid with the width list in the inner loop.
-        
-        Returns
-        -------
-        list of list of Point if nested, otherwise a flat list of Point.
-        """
-        if joint.entry_type != "crossed":
-            raise ValueError("Wrong entry type. Expected 'crossed'.")
-        spec = self._spec_cache["crossed"]
-
-        # Find offset entry points
-        strut_corners = joint.get_strut_boundary(data_type="points")
-        offset_dire = joint.calculate_screw_direction(orientation=orientation)
-        amp = spec.side_offset / math.sin(math.radians(joint.acute_angle))
-        offset_vec = offset_dire * amp
-
-        pts_entry = [p + offset_vec for p in strut_corners]
-
-        l_num = self.determine_pair_count(joint, orientation)
-        if l_num > 3:
-            raise NotImplementedError("Only support up to 3 pairs for now.")
-        
-        l = pts_entry[0].distance_to_point(pts_entry[1])
-        a2_cg = (l - spec.a2_red - spec.a1 * (l_num - 1)) / 2  # screw center to edge
-
-        vec_l = (pts_entry[1] - pts_entry[0]).unitized()
-        point_grid = []
-        for i in range(l_num):
-            pt_left = pts_entry[0] + (vec_l * a2_cg) + (vec_l * spec.a1) * i
-            pt_right = pts_entry[3] + (vec_l * a2_cg) + (vec_l * spec.a1) * i + (vec_l * spec.a2_red)
-            point_grid.append([pt_left, pt_right])
-
-        if not nested:
-            return [pt for row in point_grid for pt in row]
-        return point_grid
-
-    def populate_crossed_screw_lines(self, joint, orientation="along_cross", nested=False, restrict=True):
+    def populate_crossed_screw_lines(self, joint, point_grid, orientation="along_cross", restrict=True):
         """
         Populate screw lines from the height sides diagonally.
 
@@ -227,23 +238,23 @@ class ScrewSolver:
         ----------
         joint : JointWrapper
             The joint to populate screws on.
+        point_grid : list of list of Point
+            The entry points to create screw lines.
         orientation : str
             "perp_main", "perp_riser", "perp_cross", "bisector" and "along_cross".
-        nested : bool
-            If True, create a nested grid with the width list in the inner loop.
         restrict : bool
             If True, restrict the screw length to the predefined lengths.
         
         Returns
         -------
-        list of list of Lines if nested, otherwise a flat list of Lines.
+        list of list of Lines
+            Inner list is along the width direction.
         """
         if joint.entry_type != "crossed":
             raise ValueError("Wrong entry type. Expected 'crossed'.")
         spec = self._spec_cache["crossed"]
         screw_lengths = sorted(spec.SCREW_LENGTHS, reverse=True)
 
-        entry_grid = self.populate_crossed_entry_points(joint, orientation=orientation, nested=True)
         exit_frames = [joint.cross_beam.ref_sides[(joint.cross_beam_ref_side_index + i) % 4] for i in range(1, 4)]
 
         # Find rotate axis
@@ -251,11 +262,11 @@ class ScrewSolver:
         normal = joint.main_beam.front_side(joint.main_beam_ref_side_index).normal
         rotate_axis = cross_vectors(dire, normal)
     
-        line_list = []
-        for row in entry_grid:
+        line_grid = []
+        for row in point_grid:
             line_row = []
             for i, pt in enumerate(row):
-                sign = -1 if i == 0 else +1
+                sign = -1 if i % 2 == 0 else +1
                 side_vec = dire.rotated(sign * math.radians(spec.side_angle), rotate_axis)
 
                 exit_frame = None
@@ -278,25 +289,14 @@ class ScrewSolver:
                         continue
                     pt_exit = pt + side_vec * suitable_length
                 line_row.append(Line(pt, pt_exit))
-            line_list.append(line_row)
+            line_grid.append(line_row)
+        return line_grid
 
-        if not nested:
-            line_list = [line for row in line_list for line in row]
-        return line_list
-
-    def create_screw_cylinders(self, joint, orientation, nested=False, restrict=True):
-        if joint.entry_type == "aligned":
-            line_list = self.populate_aligned_screw_lines(joint, orientation=orientation, nested=True, restrict=restrict)
-            spec = self._spec_cache["aligned"]
-        elif joint.entry_type == "crossed":
-            line_list = self.populate_crossed_screw_lines(joint, orientation=orientation, nested=True, restrict=restrict)
-            spec = self._spec_cache["crossed"]
-
-        if nested:
-            return [[Cylinder.from_line_and_radius(line, spec.SCREW_DIAMETER / 2) for line in row] for row in line_list]
-        return [Cylinder.from_line_and_radius(line, spec.SCREW_DIAMETER / 2) for row in line_list for line in row]
-    
-    def add_drilling_features(self, joint, orientation, target="main", depth_limited=True, tol=1e-3, with_data=False):
+    def create_screw_cylinders(self, line_grid):
+        radius = ScrewSpecification.SCREW_DIAMETER / 2.0
+        return [Cylinder.from_line_and_radius(line, radius) for row in line_grid for line in row]
+                
+    def add_drilling_features(self, joint, line_grid, target="main", depth_limited=True, tol=1e-3):
         """
         Add drilling features created at the joint to the beam(s).
 
@@ -304,22 +304,15 @@ class ScrewSolver:
         ----------
         joint : JointWrapper
             The joint to add drilling features on.
-        orientation : str
-            "perp_main", "perp_riser", "perp_cross", "bisector" and "along_cross".
+        line_grid : list of list of Line
+            The screw lines to create drilling features.
         target : str
             "main", "cross" or "both" to specify which beam(s) to add drilling features on.
         depth_limited : bool
             If True, restrict the drilling depth to the predefined screw lengths.
         tol : float
             A small tolerance to move the start point away from the entry face to avoid non-intersection issue.
-        with_data : bool
-            If True, return a dictionary of "entry_points" and "screw_lines" for debugging and visualization.
         """
-        if joint.entry_type == "aligned":
-            line_list = self.populate_aligned_screw_lines(joint, orientation=orientation, nested=False, restrict=True)
-        elif joint.entry_type == "crossed":
-            line_list = self.populate_crossed_screw_lines(joint, orientation=orientation, nested=False, restrict=True)
-
         if target == "main":
             beams = (joint.main_beam,)
         elif target == "cross":
@@ -327,6 +320,7 @@ class ScrewSolver:
         elif target == "both":
             beams = (joint.main_beam, joint.cross_beam)
 
+        line_list = [line for line_row in line_grid for line in line_row]
         for line in line_list:
             # Move the start point slightly away from the entry face to avoid non-intersection issue
             line = Line(line[0] - line.direction * tol, line[1])
@@ -336,10 +330,7 @@ class ScrewSolver:
                     line, beam, ScrewSpecification.DRILLING_DIAMETER
                 )
                 beam.add_feature(drilling)
-        
-        if with_data:
-            return {"entry_points": [ln[0] for ln in line_list], "screw_lines": line_list}
-        return {}
+        return
     
 # -----------------------------------
 # Main API
@@ -347,6 +338,7 @@ class ScrewSolver:
 def apply_screws(
         model,
         spec_model="WT-plus-6.5",
+        screw_map=None,
         orientation_aligned="bisector",
         orientation_crossed="along_cross",
         drill_target="main",
@@ -380,29 +372,32 @@ def apply_screws(
 
         if joint.entry_type == "aligned":
             orientation = orientation_aligned
+            entry_point_grid = solver.populate_aligned_entry_points(joint, orientation)
+            screw_line_grid = solver.populate_aligned_screw_lines(joint, entry_point_grid, orientation, restrict=True)
+
         elif joint.entry_type == "crossed":
             orientation = orientation_crossed
+            entry_point_grid = solver.populate_crossed_entry_points(joint, orientation)
+            screw_line_grid = solver.populate_crossed_screw_lines(joint, entry_point_grid, orientation, restrict=True)
+
         else:
             raise ValueError("Unknown entry type.")
         
-        result = solver.add_drilling_features(
+        solver.add_drilling_features(
             joint,
-            orientation=orientation,
+            line_grid=screw_line_grid,
             target=drill_target,
-            depth_limited=depth_limited,
-            with_data=with_data
+            depth_limited=depth_limited
         )
         if with_data:
-            key = f"{joint.main_beam.name}_{joint.cross_beam.name}"
-            
-            strut_face = joint.get_strut_boundary(data_type="polyline")
+            interface = joint.get_interface_boundary(data_type="polyline")
             entry_face, exit_face = joint.find_screw_boundaries(orientation=orientation, data_type="polylines")
             
-            results[key] = {
-                "strut_face": strut_face,
+            results[joint.guid] = {
+                "interface": interface,
                 "entry_face": entry_face,
                 "exit_face": exit_face,
-                "entry_points": result["entry_points"],
-                "screw_lines": result["screw_lines"],
+                "entry_points": [pt for row in entry_point_grid for pt in row],
+                "screw_lines": [ln for row in screw_line_grid for ln in row],
             }
-    return results if with_data else {}
+    return results
